d2e9d983bcafee73688394e9b22e951e3518bf6b
minhkey <minhkey@pm.me>
2023-08-24 23:25:09 +0200

    Minor fixes

--- install/02_game.sh
+++ install/02_game.sh
@@ -9,7 +9,7 @@ echo ""
 # extract game
 sudo mkdir -p $GAME_PATH
 cd /home/$USER/tibia-770-setup
-sudo tar -xvzf resources/tibia-game.tar.gz -C $GAME_PATH
+sudo tar -xvzf resources/tibia-game.tar.gz -C /home
 sudo cp $GAME_PATH/bin/game $GAME_PATH/bin/game.original
 sudo cp $GAME_PATH/.tibia $GAME_PATH/.tibia.original
 
