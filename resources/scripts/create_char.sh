3d14626a0ccb1613cd75e7929974d255910e815e
--replace-all <--replace-all>
2023-09-18 15:22:33 +0200

    Daily present script

--- resources/scripts/create_char.sh
+++ resources/scripts/create_char.sh
@@ -9,7 +9,10 @@ gender=$4
 cp /home/$USER/tibia-770-setup/resources/template.usr /home/game/usr/$dir/$account_id.usr
 
 # sed charname
-sed -i "s/\"Default Character\"/\"$charname\"/g" /home/game/usr/$dir/$account_id.usr
+sed -i "s/\"Template Character\"/\"$charname\"/g" /home/game/usr/$dir/$account_id.usr
+
+# sed account id
+sed -i "s/ID              = 999999/ID              = $account_id/g" /home/game/usr/$dir/$account_id.usr
 
 # sed gender
 sed -i "s/Race            = 1/Race            = $gender/g" /home/game/usr/$dir/$account_id.usr
