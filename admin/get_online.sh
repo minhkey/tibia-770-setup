1c1ab625335a9ed43a51c1bf65b60585ca00aad7
--replace-all <--replace-all>
2023-09-19 15:40:22 +0200

    Fix online

--- admin/get_online.sh
+++ admin/get_online.sh
@@ -8,4 +8,4 @@ fi
 MYSQL_COMMAND="SELECT players_online FROM stats;"
 ONLINE=$(mysql -u $MYSQL_NAME -p$MYSQL_PASSWORD $MYSQL_NAME -e "$MYSQL_COMMAND")
 
-echo $ONLINE > /var/www/carlandia/data/online.txt
+echo $ONLINE
